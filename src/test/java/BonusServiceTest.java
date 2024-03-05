c1878eb5a5d6162df5db248b556269b2f69ed769
Vitoskaletos <usolsev87@mail.ru>
2024-03-05 23:10:29 +0300

    teste

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class BonusServiceTest {
+public class BonusServiceTest{
 
     @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
